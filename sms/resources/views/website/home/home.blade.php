591002c402ee2ca3f0fde7fda02a3597fee2e38a
Tanveerbitm <cityuniversity303@gmail.com>
2022-03-02 13:03:19 +0600

    d

---
+++ sms/resources/views/website/home/home.blade.php
@@ -0,0 +1,130 @@
+@extends('master.front.master')
+
+@section('body')
+  <section class="py-5">
+      <div class="container">
+          <div class="row">
+              <div class="col-md-6">
+                  <div class="card mb-3">
+                      <div class="row g-0">
+                          <div class="col-md-5">
+                              <img src="{{asset('/img/php.jpg')}}" class="img-fluid h-100" alt="...">
+                          </div>
+                          <div class="col-md-7">
+                              <div class="card-body">
+                                  <figure>
+                                      <blockquote class="blockquote">
+                                          <p>A well-known quote, contained in a blockquote element.</p>
+                                      </blockquote>
+                                      <figcaption class="blockquote-footer">
+                                          Someone famous in <cite title="Source Title">Source Title</cite>
+                                      </figcaption>
+                                  </figure>
+                                  <ul>
+                                      <li>Course Feature One</li>
+                                      <li>Course Feature Two</li>
+                                      <li>Course Feature Three</li>
+                                      <li>Course Feature Four</li>
+                                  </ul>
+                                  <p class="card-text"><small class="text-muted">Last date of admission</small></p>
+                                  <a href="" class="btn btn-success">Read More</a>
+                                  <a href="" class="btn btn-success float-end">Apply Now</a>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              <div class="col-md-6">
+                  <div class="card mb-3">
+                      <div class="row g-0">
+                          <div class="col-md-5">
+                              <img src="{{asset('/img/php.jpg')}}" class="img-fluid h-100" alt="...">
+                          </div>
+                          <div class="col-md-7">
+                              <div class="card-body">
+                                  <figure>
+                                      <blockquote class="blockquote">
+                                          <p>A well-known quote, contained in a blockquote element.</p>
+                                      </blockquote>
+                                      <figcaption class="blockquote-footer">
+                                          Someone famous in <cite title="Source Title">Source Title</cite>
+                                      </figcaption>
+                                  </figure>
+                                  <ul>
+                                      <li>Course Feature One</li>
+                                      <li>Course Feature Two</li>
+                                      <li>Course Feature Three</li>
+                                      <li>Course Feature Four</li>
+                                  </ul>
+                                  <p class="card-text"><small class="text-muted">Last date of admission</small></p>
+                                  <a href="" class="btn btn-success">Read More</a>
+                                  <a href="" class="btn btn-success float-end">Apply Now</a>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              <div class="col-md-6">
+                  <div class="card mb-3">
+                      <div class="row g-0">
+                          <div class="col-md-5">
+                              <img src="{{asset('/img/php.jpg')}}" class="img-fluid h-100" alt="...">
+                          </div>
+                          <div class="col-md-7">
+                              <div class="card-body">
+                                  <figure>
+                                      <blockquote class="blockquote">
+                                          <p>A well-known quote, contained in a blockquote element.</p>
+                                      </blockquote>
+                                      <figcaption class="blockquote-footer">
+                                          Someone famous in <cite title="Source Title">Source Title</cite>
+                                      </figcaption>
+                                  </figure>
+                                  <ul>
+                                      <li>Course Feature One</li>
+                                      <li>Course Feature Two</li>
+                                      <li>Course Feature Three</li>
+                                      <li>Course Feature Four</li>
+                                  </ul>
+                                  <p class="card-text"><small class="text-muted">Last date of admission</small></p>
+                                  <a href="" class="btn btn-success">Read More</a>
+                                  <a href="" class="btn btn-success float-end">Apply Now</a>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              <div class="col-md-6">
+                  <div class="card mb-3">
+                      <div class="row g-0">
+                          <div class="col-md-5">
+                              <img src="{{asset('/img/php.jpg')}}" class="img-fluid h-100" alt="...">
+                          </div>
+                          <div class="col-md-7">
+                              <div class="card-body">
+                                  <figure>
+                                      <blockquote class="blockquote">
+                                          <p>A well-known quote, contained in a blockquote element.</p>
+                                      </blockquote>
+                                      <figcaption class="blockquote-footer">
+                                          Someone famous in <cite title="Source Title">Source Title</cite>
+                                      </figcaption>
+                                  </figure>
+                                  <ul>
+                                      <li>Course Feature One</li>
+                                      <li>Course Feature Two</li>
+                                      <li>Course Feature Three</li>
+                                      <li>Course Feature Four</li>
+                                  </ul>
+                                  <p class="card-text"><small class="text-muted">Last date of admission</small></p>
+                                  <a href="" class="btn btn-success">Read More</a>
+                                  <a href="" class="btn btn-success float-end">Apply Now</a>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </section>
+@endsection
